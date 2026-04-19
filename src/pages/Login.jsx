4b031fe6a122d458e1b3e3cdc4d7fdf16d8f5d32
Anikate Kanoo <anikate955@gmail.com>
2026-04-20 02:50:59 +0530

    fix: update API URL in .env and ensure .env is ignored in .gitignore

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom'
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { apiFetch } from "../utils/api";
+import { apiFetch } from "../utils/api.js";
 
 const Login = () => {
   const [form, setForm] = useState({
